b332a93f09793c618bea8afa75919f972ab964ce
gtg7784 <tae.gun7784@gmail.com>
2021-08-16 00:09:06 +0900

    add [POST] /classify

--- application.py
+++ application.py
@@ -9,6 +9,10 @@ from werkzeug.utils import secure_filename
 from PIL import Image, ImageDraw, ImageFont
 from videoprops import get_video_properties
 from nudenet import NudeClassifier
+import base64
+import string
+import random
+from io import BytesIO
 
 app = Flask(__name__)
 CORS(app)
@@ -19,9 +23,59 @@ classifier = NudeClassifier()
 def home():
     return 'hello'
 
-@app.route('/static/<path:path>', methods=['GET'])
-def static_dir(path):
-    return send_from_directory('static', path)
+@app.route('/classify', methods=['POST'])
+def b64_image_inference():
+    b64_data = request.json['image']
+
+    b64_image = b64_data.split(',')[1]
+    image_info = b64_data.split(',')[0]
+    image_type = image_info.split('image/')[1][:-7]
+
+    ascii_letters = list(string.ascii_letters)
+    random.shuffle(ascii_letters)
+    filedata = base64.b64decode(b64_image)
+    filename = ''.join(ascii_letters[:8]) + '.' + image_type
+    filepath = f"./files/{filename}"
+    with open(filepath, 'wb') as f:
+        f.write(filedata)
+
+    preds = classifier.classify(filepath, batch_size=32)
+
+    pred = preds.get(filepath)
+
+    if(pred['safe'] > pred['unsafe']):
+        result_image = b64_image
+    else:
+        img = cv2.imread(filepath)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        dst = cv2.blur(img,(250, 250))
+        dstImg = Image.fromarray(dst)
+        draw = ImageDraw.Draw(dstImg)
+        font = ImageFont.truetype('./font/SpoqaHanSansNeo-Regular.ttf', 60)
+        msg = '검열된 이미지'
+        W, H = dstImg.width, dstImg.height
+        w, h = draw.textsize(msg, font=font)
+        draw.text(((W-w)/2, (H-h)/2), msg, (255, 0, 0), font=font)
+
+        buffered = BytesIO()
+        dstImg.save(buffered, format="JPEG")
+        result_image = base64.b64encode(buffered.getvalue()).decode()
+
+        print(result_image)
+
+    response = {
+        'image': image_info +',' + result_image,
+        'pred': pred
+    }
+
+    os.remove(filepath)
+
+    response = app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.route('/upload/files', methods=['POST'])
 def image_inference():
